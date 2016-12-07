c895c919ef15427b2d137ed6ddf1b0aa9d87a4ea
green4free <ninjalostinsnow@gmail.com>
2016-12-07 19:19:21 +0100

    Extraproblem day1

--- day1/day1.py
+++ day1/day1.py
@@ -1,16 +1,16 @@
 from math import *
-step = pi / 2.0
+step = 90
 deg = step
 pos = [0,0]
 f = open("input.txt")
-a = f.readline().split(',')
+a = f.readline().split(', ')
 f.close()
 for t in a:
 	if t[0] == 'R':
 		deg -= step
 	elif t[0] == 'L':
 		deg += step
-	pos[0] += int(cos(deg)) * int(t[1:])
-	pos[1] += int(sin(deg)) * int(t[1:])
+	pos[0] += int(cos(radians(deg))) * int(t[1:])
+	pos[1] += int(sin(radians(deg))) * int(t[1:])
 
 print abs(pos[0]) + abs(pos[1])
